3a2d381e836a3de82076d3899d9ab453ae869043
nneo <aisapple1030@naver.com>
2022-07-17 19:47:25 +0900

    mp4 to wav 완료

--- sookpeech_analysis/analysis/views.py
+++ sookpeech_analysis/analysis/views.py
@@ -1,25 +1,35 @@
 from django.shortcuts import render
-import boto3
-import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+import boto3
+import json
+import subprocess
+from moviepy.editor import *
 
 @method_decorator(csrf_exempt, name='dispatch')
 def analysis(request, user_id, rand):
     try: 
-        # TODO: 1) s3에서 영상 가져오기
+        # TODO: 1) s3에서 영상 가져온 후 mp4 저장시키기
         s3_resource = boto3.resource('s3')
         bucket = s3_resource.Bucket(name='sookpeech-wavfile')
-        res = bucket.Object(f'video_{user_id}_{rand}.mp4').get()
-        print(res)
+        video_bytes = bucket.Object(f'video_{user_id}_{rand}.mp4').get()['Body'].read() # bytes type
+
+        f = open(f'./analysis/mp4s/{user_id}_{rand}.mp4', "wb")
+        f.write(video_bytes)
+        f.close()
         # TODO: 2) 영상에서 음성 추출
+        command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(f'./analysis/mp4s/{user_id}_{rand}.mp4', f'./analysis/wavs/{user_id}_{rand}.wav', format="wav")
+        subprocess.call(command, shell=True)
+
         # TODO: 3) 영상 분석
+        
         # TODO: 4) 음성 분석
         return HttpResponse(json.dumps({
             'response': 'success'
         }))
     except Exception as e:
+        print(e)
         return HttpResponse(json.dumps({
-            'response': 'fail'
+            'response': 'fail to access to s3 files'
         }))
