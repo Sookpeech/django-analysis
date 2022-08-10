6c00c27beeda30acfbd41eed2330aaff6343a2e0
nneo <aisapple1030@naver.com>
2022-08-11 04:27:16 +0900

    Heroku: settings.py 수정

--- sookpeech_analysis/sookpeech_analysis/settings.py
+++ sookpeech_analysis/sookpeech_analysis/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
